03ab7fbba43f28aa04cbf074ae926f39d9bafc02
ChetN343 <77791728+ChetN343@users.noreply.github.com>
2022-09-24 13:40:25 +0530

    Added Sectors and Intent

---
+++ Intent.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import csv
+
+
+target = "intent_datasets.csv"
+
+
+intent_all = pd.DataFrame()
+for i in range(1, 51):
+
+    
+    Intent = pd.DataFrame()
+
+    
+
+    Threat_Category = ['Criminal', 'Criminal', 'Governmental',
+                        'Governmental', 'Commercial', 'Individual', 'Individual']
+
+    ID = [i for j in range(len(Threat_Category))]
+    
+    Intent["Dataset_ID"] = ID
+
+    Intent['Threat Category'] = Threat_Category
+
+    Threat_Actor = ['Organized Crime', 'Other Criminals', 'APTs',
+                    'Other Governments', 'Competitors', 'Anarchist Insider', 'Anarchist Outsider']
+    Intent['Threat Actor'] = Threat_Actor
+
+    Intent['CC1'] = np.random.randint(1, 6, size=len(Intent))
+    Intent['CC2'] = np.random.randint(1, 6, size=len(Intent))
+    Intent['CC3'] = np.random.randint(1, 6, size=len(Intent))
+    Intent['CC4'] = np.random.randint(1, 6, size=len(Intent))
+    Intent['CC5'] = np.random.randint(1, 6, size=len(Intent))
+    Intent['CC6'] = np.random.randint(1, 6, size=len(Intent))
+    Intent['CC7'] = np.random.randint(1, 6, size=len(Intent))
+    Intent['CC8'] = np.random.randint(1, 6, size=len(Intent))
+    Intent['CC9'] = np.random.randint(1, 6, size=len(Intent))
+    intent_all = pd.concat([intent_all, Intent])
+
+intent_all.reset_index(inplace=True, drop=True)
+intent_all.head()
+intent_all.tail()
+
+intent_all.to_csv(target)
---
+++ Sectors.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import csv
+
+
+
+target = "sectors_datasets.csv"
+
+sectors_all = pd.DataFrame()
+
+for i in range(1,51):
+
+    Sector = pd.DataFrame()
+
+    Threat_Category = ['Criminal', 'Criminal', 'Governmental', 'Governmental', 'Commercial', 'Individual', 'Individual']
+
+    ID = [i for j in range(len(Threat_Category))]
+    Sector["Dataset_ID"] = ID
+    Sector['Threat Category'] = Threat_Category
+
+    Threat_Actor = ['Organized Crime', 'Other Criminals', 'APTs', 'Other Governments', 'Competitors', 'Anarchist Insider', 'Anarchist Outsider']
+    Sector['Threat Actor'] = Threat_Actor
+
+
+    Sector['IT'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Healthcare'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Finance'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Consumer Discretionary'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Communication Services'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Industrials'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Consumer Staples'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Energy'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Utilities'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Real Estate'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Materials'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Government'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['SLED'] = np.random.randint(1, 6, size=len(Sector))
+    Sector['Other Critical'] = np.random.randint(1, 6, size=len(Sector))
+
+    sectors_all = pd.concat([sectors_all, Sector])
+
+sectors_all.reset_index(inplace=True, drop=True)
+sectors_all.head()
+sectors_all.tail()
+
+sectors_all.to_csv(target)
+
